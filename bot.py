c6a70b4d0ea337ca35d6c3b86d46e1337cf80b80
2w1nxdd <user210307@gmail.com>
2024-06-09 16:10:40 +0300

    energy, some fixes, improvements, basic functions

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("token")
 
 web_app_url = "t.me/hamstercombat_copy_bot/webapp"
 
-text = "Это приложение - копия гры Hamster Kombat, сделанная за 100 минут!\n\n Нажмите на кнопку снизу, что запустить его!"
+text = "Это приложение - копия гры Hamster Kombat, сделанная за 100 минут!\n\nНажмите на кнопку снизу, что запустить его!"
 
 button = types.InlineKeyboardButton('Запустить', url=web_app_url)
 keyboard = types.InlineKeyboardMarkup()
--- script.js
+++ script.js
@@ -1,16 +1,76 @@
 let button = document.querySelector(".button");
 let countTxt = document.querySelector(".count__txt");
 
-if (localStorage.getItem("coin-count") === null) {
-    localStorage.setItem("coin-count", 0);
+let tapIncomeTxt = document.querySelector(".tap-income");
+let coinsForUpTxt = document.querySelector(".coins-for-up");
+let hourIncomeTxt = document.querySelector(".hour-income")
+
+let currentEnergyTxt = document.querySelector(".current-energy");
+let maxEnergyTxt = document.querySelector(".max-energy");
+
+let maxEnergy = 1000;
+let coinsForUpgrade = 1000;
+
+function fillLocalStorage(key) {
+    if (localStorage.getItem(key) === null) {
+        localStorage.setItem(key, 0);
+    }
+
 }
+
+if (localStorage.getItem("tap-income") === null) {
+    localStorage.setItem("tap-income", 1);
+}
+
+if (localStorage.getItem("current-energy") === null) {
+    localStorage.setItem("current-energy", maxEnergy);
+}
+
+if (localStorage.getItem("coins-for-up") === null) {
+    localStorage.setItem("coins-for-up", 1000);
+}
+
+
+setInterval(function refillEnergy() {
+    let currentEnergy = +localStorage.getItem("current-energy");
+    if (currentEnergy <= maxEnergy - 3) {
+        currentEnergy += 3;
+        localStorage.setItem("current-energy", currentEnergy);
+        currentEnergyTxt.textContent = localStorage.getItem("current-energy")
+    } else {
+        localStorage.setItem("current-energy", maxEnergy);
+        currentEnergyTxt.textContent = localStorage.getItem("current-energy")
+    }
+}, 3000)
+
+currentEnergyTxt.textContent = localStorage.getItem("current-energy")
+
+fillLocalStorage("coin-count");
+fillLocalStorage("hour-income");
+
 
 countTxt.textContent = localStorage.getItem("coin-count");
+tapIncomeTxt.textContent = localStorage.getItem("tap-income");
+coinsForUpTxt.textContent = localStorage.getItem("coins-for-up");
+hourIncomeTxt.textContent = localStorage.getItem("hour-income");
 
-button.onclick = function () {
-    let count = localStorage.getItem("coin-count");
-    count++;
-    localStorage.setItem("coin-count", count)
-    countTxt.textContent = localStorage.getItem("coin-count");
 
-}
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (+localStorage.getItem("current-energy") >= localStorage.getItem("tap-income")) {
+        let currentEnergy = +localStorage.getItem("current-energy");
+        currentEnergy -= +localStorage.getItem("tap-income");
+        currentEnergyTxt.textContent = currentEnergy;
+        localStorage.setItem("current-energy", currentEnergy);
+
+
+        let count = +localStorage.getItem("coin-count");
+        count += +localStorage.getItem("tap-income");
+        localStorage.setItem("coin-count", count)
+        countTxt.textContent = localStorage.getItem("coin-count");
+    }
+
+
+});
